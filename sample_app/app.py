03eba2e4ded19fbff8fa85ff753d85b2df878a63
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2021-07-08 10:02:35 +0530

    Common format

--- sample_app/app.py
+++ sample_app/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 from flask_pwa import PWA
 
 
-def create_app():
-    app = Flask(__name__)
-    PWA(app)
+app = Flask(__name__)
+PWA(app)
 
-    @app.route("/")
-    def index():
-        return render_template("index.html")
+@app.route("/")
+def index():
+	return render_template("index.html")
 
-    return app
+if __name__ == '__main__':
+	app.run()
